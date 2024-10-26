c52a9f7f46fb428888cc55c9043f18eca771dcfc
Pradhunyagawande01 <pradhumnya.gawande.cyb@ghrietn.raisoni.net>
2024-10-26 13:28:03 +0530

    new commit added

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,14 @@ const App = () => {
   return (
     <div className='flex items-center justify-center h-screen bg-[#ECE7E2] flex-col'>
       <div>
-      <img src={myimg} alt="My Image" className="w-32 h-32" />
+       <img src={myimg} alt="My Image" className="w-32 h-32" />
       </div>
       <h1 className='font-bold text-[#4A7766] text-2xl'>
         Welcome to My Portfolio Website
       </h1>
+      <p className='text-[#4A7766] text-lg'>
+        I am a Frontend Developer
+      </p>
     </div>
   );
 }
