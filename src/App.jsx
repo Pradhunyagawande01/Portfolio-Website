ab7b3c21f8dd2f2c979a83246b61fc698ed704df
Pradhunyagawande01 <pradhumnya.gawande.cyb@ghrietn.raisoni.net>
2025-01-25 15:00:30 +0530

    Feat: add the new data

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-[#273E47] text-white">
+      <h1 className="text-4xl">Hello,Prada!</h1>
+    </div>
+  )
+}
+
+export default App
